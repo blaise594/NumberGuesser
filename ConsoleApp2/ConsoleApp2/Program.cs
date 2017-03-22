b9e541aaceb87bcbab39589bccb1738ea47933da
blaise594 <danielbrogers594@gmail.com>
2017-03-22 15:05:02 -0400

    created basic number guesser game

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var target = new Random().Next(1, 101);
+            Console.WriteLine($"the target is {target}");
+
+           Console.WriteLine($"Guess now");
+            var counter = 0;
+            while (counter < 5)
+            {
+                var input = Console.ReadLine();
+                var guess = 0;
+                int.TryParse(input, out guess);
+
+                Console.WriteLine("You guessed {guess}");
+
+
+                if (guess < target)
+                {
+                    Console.WriteLine("Your guess was too low");
+                }
+                else if (guess > target)
+                {
+                    Console.WriteLine("Your guess was too high");
+                }
+            }
+        }
+
+    }
+
+}
